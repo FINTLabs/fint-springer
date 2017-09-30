45f412df25c6ab829cfad6011e7c1b9315dce565
jarlehansen <jarle@jarlehansen.net>
2017-09-30 12:24:31 +0200

    updated AdminControllerSpec

--- src/test/groovy/no/fint/springer/AdminControllerSpec.groovy
+++ src/test/groovy/no/fint/springer/AdminControllerSpec.groovy
@@ -1,10 +1,8 @@
 package no.fint.springer
 
 import no.fint.test.utils.MockMvcSpecification
-import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.servlet.MockMvc
 
-@SpringBootTest(classes = TestApplication)
 class AdminControllerSpec extends MockMvcSpecification {
     private MockMvc mockMvc
     private ExampleDataLoader exampleDataLoader
