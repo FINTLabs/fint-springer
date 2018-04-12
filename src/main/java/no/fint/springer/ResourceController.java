77a23121a20d3ab7ec87fed22643c06aaf0c2d3f
Asgeir Nilsen <asgeir@fintprosjektet.no>
2018-04-12 13:57:36 +0200

    Updated stack, made Jackson serialization inclusion configurable.

--- src/main/java/no/fint/springer/ResourceController.java
+++ src/main/java/no/fint/springer/ResourceController.java
@@ -1,6 +1,10 @@
 package no.fint.springer;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,11 +21,18 @@ public class ResourceController {
     @Autowired
     private MongoTemplate mongoTemplate;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Value("${fint.springer.serialization.include:ALWAYS}")
+    private JsonInclude.Include include;
+
     @GetMapping("/{collection}")
     public ResponseEntity getResource(@PathVariable String collection) {
         try {
             Class<?> modelClass = SpringerModels.getModelClass(collection);
             List<?> values = mongoTemplate.find(null, modelClass, collection);
+            objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS).setSerializationInclusion(include);
             return ResponseEntity.ok(values);
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().body(String.format("The collection %s was not found", collection));
